22dfb02cec1255f0db397048e51bd815fcad0570
Sébastien Coze <96086580+SebastienCozeDev@users.noreply.github.com>
2024-04-14 15:48:10 +0200

    [DONE] Create the layout (#3)

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import {Text, View} from "react-native";
+import React from "react";
+import {style} from "./App.style";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>My application</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={style.app}>
+          <View style={style.header}>
+            <Text>Header</Text>
+          </View>
+          <View style={style.body}>
+            <Text>Body</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={style.footer}>
+        <Text>Footer</Text>
+      </View>
+    </>
   );
 }
